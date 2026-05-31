8887a571c495ad76ddb48ddab95dc8922f433bb0
dashygo097 <dashygo097@gmail.com>
2026-05-31 17:06:47 +0800

    [risc] graph: decoder

---
+++ arch/node/decoder/Decoder.scala
@@ -0,0 +1,38 @@
+package arch.node.decoder
+
+import arch.configs._
+import vutils.graph.{ Node, NodeConfig, NodeSelector, NodeType }
+
+class Decoder(implicit p: Parameters) extends Node(new DecoderIO) {
+  private val cfg = NodeConfig(
+    selector = NodeSelector(
+      DecoderDims.ISA  -> p(ISA).name,
+      DecoderDims.KIND -> p(DecoderKind)
+    )
+  )
+
+  override def nodeType: NodeType  = DecoderMeta.Type
+  override def desiredName: String = s"decoder_${cfg.selector.canonicalName}"
+
+  private val isaImpl  = DecoderIsaFactory.select(cfg)
+  private val kindImpl = DecoderKindFactory.select(cfg)
+
+  for (w <- 0 until p(IssueWidth))
+    io.decode.out(w) := kindImpl.decode(isaImpl, io.decode.instr(w))
+}
+
+import arch.node.imm.ImmInit
+import vutils._
+
+object DecoderNode extends App {
+  ImmInit
+  DecoderInit
+
+  DesignEmitter.emit(
+    gen = new Decoder,
+    filename = "decoder",
+    target = SystemVerilog,
+    info = true,
+    lowering = true,
+  )
+}
