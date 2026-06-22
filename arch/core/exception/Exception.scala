c823760c8b922bfb24fc94e6f8f93b3d77553928
dashygo097 <dashygo097@gmail.com>
2026-06-22 15:56:47 +0800

    [risc] pruning signals io interface

--- arch/core/exception/Exception.scala
+++ arch/core/exception/Exception.scala
@@ -5,6 +5,7 @@ import arch.core.ifu.IfuExceptionResp
 import arch.core.rob.RobExceptionResp
 import vutils.graph.{ Node, NodeConfig, NodeSelector }
 import chisel3._
+import chisel3.util.PriorityEncoder
 
 class Exception(implicit p: Parameters) extends Node[Parameters]("exception") {
   override protected def cfg: NodeConfig = NodeConfig(
@@ -13,8 +14,12 @@ class Exception(implicit p: Parameters) extends Node[Parameters]("exception") {
     )
   )
 
-  val redirect  = in[ExceptionRedirectReq]
-  val sync      = in[ExceptionSyncReq]
+  val committedSync = inVec[ExceptionSyncReq](p => p(CommitWidth))
+  val sync          = out[ExceptionSyncReq]
+
+  val committedRedirect = inVec[ExceptionRedirectReq](p => p(CommitWidth))
+  val redirect          = out[ExceptionRedirectReq]
+
   val async     = in[ExceptionAsyncReq]
   val csrStatus = in[ExceptionCsrStatus]
 
@@ -27,9 +32,31 @@ class Exception(implicit p: Parameters) extends Node[Parameters]("exception") {
   private val isaImpl = ExceptionIsaFactory.select(cfg)
   private val archPc  = Mux(robResp.in.empty, ifuResp.in.fetch_pc, robResp.in.commit_pc)
 
+  // Sync logic
+  private val syncValidVec = VecInit(
+    (0 until p(CommitWidth)).map(w => committedSync.in.lanes(w).valid)
+  )
+  private val syncAny      = syncValidVec.asUInt.orR
+  private val syncSlot     = PriorityEncoder(syncValidVec.asUInt)
+
+  sync.out.valid  := syncAny
+  sync.out.kind   := committedSync.in.lanes(syncSlot).kind
+  sync.out.target := committedSync.in.lanes(syncSlot).target
+  sync.out.pc     := committedSync.in.lanes(syncSlot).pc
+
+  // Redirect logic
+  private val redirectValidVec = VecInit(
+    (0 until p(CommitWidth)).map(w => committedRedirect.in.lanes(w).valid)
+  )
+  private val redirectAny      = redirectValidVec.asUInt.orR
+  private val redirectSlot     = PriorityEncoder(redirectValidVec.asUInt)
+
+  redirect.out.valid  := !syncAny && redirectAny
+  redirect.out.target := committedRedirect.in.lanes(redirectSlot).target
+
   private val selected = isaImpl.select(
-    redirect = redirect.in,
-    sync = sync.in,
+    redirect = redirect.out,
+    sync = sync.out,
     async = async.in,
     csrBusy = csrStatus.in.busy,
     archPc = archPc
--- arch/core/flush/Flush.scala
+++ arch/core/flush/Flush.scala
@@ -1,48 +1,20 @@
 package arch.core.flush
 
 import arch.configs._
-import arch.core.exception.{ ExceptionCsrReq, ExceptionFlushReq, ExceptionRedirectReq, ExceptionSyncReq }
+import arch.core.exception.{ ExceptionCsrReq, ExceptionFlushReq }
 import arch.core.ifu.IfuExceptionReq
 import vutils.graph.Node
 import chisel3._
-import chisel3.util.PriorityEncoder
 
 class Flush(implicit p: Parameters) extends Node[Parameters]("flush") {
-  val committedRedirect = inVec[ExceptionRedirectReq](p => p(CommitWidth))
-  val committedSync     = inVec[ExceptionSyncReq](p => p(CommitWidth))
-  val exception         = in[ExceptionFlushReq]
-
+  val exception   = in[ExceptionFlushReq]
   val globalFlush = out[Bool]
-  val sync        = out[ExceptionSyncReq]
-  val redirect    = out[ExceptionRedirectReq]
 
   val ifuReq    = out[IfuExceptionReq]
   val fuPoolReq = out[ExceptionCsrReq]
 
   // Global flush
   globalFlush.out := exception.in.valid
-
-  // Sync logic
-  private val syncValidVec = VecInit(
-    (0 until p(CommitWidth)).map(w => committedSync.in.lanes(w).valid)
-  )
-  private val syncAny      = syncValidVec.asUInt.orR
-  private val syncSlot     = PriorityEncoder(syncValidVec.asUInt)
-
-  sync.out.valid  := syncAny
-  sync.out.kind   := committedSync.in.lanes(syncSlot).kind
-  sync.out.target := committedSync.in.lanes(syncSlot).target
-  sync.out.pc     := committedSync.in.lanes(syncSlot).pc
-
-  // Redirect logic
-  private val redirectValidVec = VecInit(
-    (0 until p(CommitWidth)).map(w => committedRedirect.in.lanes(w).valid)
-  )
-  private val redirectAny      = redirectValidVec.asUInt.orR
-  private val redirectSlot     = PriorityEncoder(redirectValidVec.asUInt)
-
-  redirect.out.valid  := !syncAny && redirectAny
-  redirect.out.target := committedRedirect.in.lanes(redirectSlot).target
 
   ifuReq.out.redirect := exception.in.valid
   ifuReq.out.target   := exception.in.target
